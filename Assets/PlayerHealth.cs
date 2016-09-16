294dd0277f522457766c5c2e9bf64f9cb56488d4
Jordan CLEMENT <Jordan CLEMENT>
2016-09-16 23:07:06 +0200

    HUD

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int health = 100;
+
+    public GUIText playerHealth;
+    public Vector3 offset = new Vector3();
+
+	void Start ()
+    {
+	}
+	
+	void Update ()
+    {
+        playerHealth.text = health + "%";
+
+        playerHealth.transform.position = Camera.main.WorldToViewportPoint(transform.position + offset);
+	}
+}
