50535080e7f5e2e243488d071fda37401075a5fe
Jordan CLEMENT <jordan.clement@bellecour.local>
2016-09-19 10:00:01 +0200

    Mirror

---
+++ Assets/Mirror.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mirror : MonoBehaviour {
+
+	private Vector3 positionEnter;
+	public float offset;
+
+
+	void OnTriggerEnter (Collider other)
+	{
+		positionEnter = other.gameObject.transform.position;
+
+		if (gameObject.tag == "RightBoundary")
+		{
+			other.gameObject.transform.position = new Vector3 (-positionEnter.x, positionEnter.y, positionEnter.z) + new Vector3 (offset, 0, 0);
+		}
+
+		else if (gameObject.tag == "LeftBoundary")
+		{
+			other.gameObject.transform.position = new Vector3 (-positionEnter.x, positionEnter.y, positionEnter.z) - new Vector3 (offset, 0, 0);
+		}
+
+		else if (gameObject.tag == "UpBoundary")
+		{
+			other.gameObject.transform.position = new Vector3 (positionEnter.x, -positionEnter.y, positionEnter.z) + new Vector3 (0, offset, 0);
+		}
+
+		else if (gameObject.tag == "DownBoundary")
+		{
+			other.gameObject.transform.position = new Vector3 (positionEnter.x, -positionEnter.y, positionEnter.z) - new Vector3 (0, offset, 0);
+		}
+	}
+
+}
---
+++ Assets/PlayerControler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControler : MonoBehaviour {
+
+	Rigidbody rb;
+	public float speed;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+
+	{
+		float move_horiz = Input.GetAxis ("Horizontal");
+		float move_vert = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (move_horiz, move_vert, 0.0f);
+
+		rb.velocity = movement * speed;
+
+	}
+}
